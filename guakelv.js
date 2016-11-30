cd3806acfae060f04995bedff7fafafa8e28e8c6
abcdGJJ <1392552862@qq.com>
2016-11-30 20:24:05 +0800

    更详细的挂科判断--比较简陋

--- guakelv.js
+++ guakelv.js
@@ -40,7 +40,7 @@ o.reduce = function(key, values) {
 // 过滤出挂科信息
 // 由于oriScore字段是字符型，只能使用这种简陋的方式来实现（$where太费时）
 o.query = {
-    $or: [{$and: [{ "oriScore": { $lt: "60"}}, { "oriScore": { $ne: "100"}}]}, {"oriScore": "7"}, {"oriScore": "8"}, { "oriScore": "9"}]
+    $or: [{$and: [{ "oriScore": { $lt: "60"}}, { "oriScore": { $ne: "100"}}]}, {"oriScore": "7"}, {"oriScore": "8"}, { "oriScore": "9"}, { "oriScore": "6.3"}, { "oriScore": "6.4"}, { "oriScore": "6.5"}, { "oriScore": "6.6"},{ "oriScore": "6.7"},{ "oriScore": "6.8"},{ "oriScore": "6.9"}, { "oriScore": "7.1"},{ "oriScore": "7.2"},{ "oriScore": "7.3"},{ "oriScore": "7.4"},{ "oriScore": "7.5"},{ "oriScore": "7.6"},{ "oriScore": "7.7"},{ "oriScore": "7.8"},{ "oriScore": "7.9"},{ "oriScore": "8.1"},{ "oriScore": "8.2"},{ "oriScore": "8.3"},{ "oriScore": "8.4"},{ "oriScore": "8.5"},{ "oriScore": "8.6"},{ "oriScore": "8.7"},{ "oriScore": "8.8"},{ "oriScore": "8.9"},{ "oriScore": "9.1"},{ "oriScore": "9.2"},{ "oriScore": "9.3"},{ "oriScore": "9.4"},{ "oriScore": "9.5"},{ "oriScore": "9.6"},{ "oriScore": "9.7"},{ "oriScore": "9.8"},{ "oriScore": "9.9"},{"oriScore": "不合格"}]
 }
 o.out = 'fail' // 输出的表名
 Score.mapReduce(o, function(err, res) {
